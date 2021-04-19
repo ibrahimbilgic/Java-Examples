13e9b591d967c3de0c9eb0f63edaea6fcc5025e8
ibrahimbilgic <ibrahimbilgicx@gmail.com>
2021-04-19 21:47:21 +0300

    Eklendi

--- IlkPencere/SoruDeneme.java
+++ IlkPencere/SoruDeneme.java
@@ -4,6 +4,7 @@ import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 
@@ -11,17 +12,23 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 public class SoruDeneme extends JPanel implements ActionListener{
 
-    JLabel soruMetniLabel;
+    JLabel soruMetniLabel,skorLabel;
     JRadioButton cevapA,cevapB,cevapC,cevapD;
     JButton onayButton;
     ButtonGroup cevaplar;
     
     Soru[] sorular = new Soru[5];
     int gecerliSoru = 0;
+    int skor = 0;
+    int maxSoru = 5;
 
     public SoruDeneme() {
+
+        //Elemanları oluşturma kısmı..
+
         
         soruMetniLabel = new JLabel();
+        skorLabel = new JLabel("Skor: " + skor);
         cevapA = new JRadioButton();
         cevapB = new JRadioButton();
         cevapC = new JRadioButton();
@@ -36,17 +43,71 @@ public class SoruDeneme extends JPanel implements ActionListener{
 
         onayButton.addActionListener(this);
         add(soruMetniLabel);
+        
         add(cevapA);
         add(cevapB);
         add(cevapC);
         add(cevapD);
 
         add(onayButton);
+
+        add(skorLabel);
+        //Soruları oluşturma
+        sorularıOlustur();
+        soruyuGoster(gecerliSoru);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
+        boolean cevapDogruMu;
+        if(cevapA.isSelected())
+        {
+            cevapDogruMu = cevapKontrol(Soru.Cevap.CEVAP_A);
+            cevapA.setSelected(true);
+        }
+        else if(cevapB.isSelected())
+        {
+            cevapDogruMu = cevapKontrol(Soru.Cevap.CEVAP_B);
+            cevapB.setSelected(false);
+        }
+        else if(cevapC.isSelected())
+        {
+            cevapDogruMu = cevapKontrol(Soru.Cevap.CEVAP_C);
+            cevapC.setSelected(false);
+        }
+        else if(cevapD.isSelected())
+        {
+            cevapDogruMu = cevapKontrol(Soru.Cevap.CEVAP_D);
+            cevapD.setSelected(false);
+        }
+        else
+        {
+            cevapDogruMu = false;
+        }
+        if(cevapDogruMu)
+        {
+            JOptionPane.showMessageDialog(this, "Doğru cevap verdiniz.");
+            skor+=10;
+            skorLabel.setText("Skor: "+ skor);
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(this, "Yanlış cevap verdiniz.");
+            skor-=10;
+            skorLabel.setText("Skor: "+ skor);
+        }
+
+        gecerliSoru++;
+        if(gecerliSoru < maxSoru)
+        {
+            soruyuGoster(gecerliSoru);
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(this, "oyun bitti. Skorun: "+skor);
+        }
+        
+        
         
     }
     public void sorularıOlustur(){
@@ -70,9 +131,7 @@ public class SoruDeneme extends JPanel implements ActionListener{
     }
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        SoruDeneme sd = new SoruDeneme();
-        sd.sorularıOlustur();
-        sd.soruyuGoster(0);
+        SoruDeneme sd = new SoruDeneme();        
         frame.add(sd);
         frame.setVisible(true);
         frame.setSize(640,480);
